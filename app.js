a595f5070e988e86798070d139eb1286a7f80b27
Dominik Filip <filu4444@gmail.com>
2022-08-31 15:12:41 +0200

    changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(sessions({
   secret: process.env.SESSION_SECRET,
   saveUninitialized: false,
-  cookie: { maxAge: halfDay, 
+  cookie: { maxAge: 1000 * 60 * 60 * 12, 
     sameSite: true,
     secure: false
   },
