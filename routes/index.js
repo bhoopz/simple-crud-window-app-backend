1fceb4c3f6596b0893068ddeb21f6fb412ff9d77
Dominik Filip <filu4444@gmail.com>
2022-08-31 15:25:46 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
+const { route } = require('../app');
 var router = express.Router();
-const {displayIndex, deleteRecord, editRecord, addRecord, register} = require('../controllers/indexController');
+const {displayIndex, deleteRecord, editRecord, addRecord, register, login} = require('../controllers/indexController');
 
 /* GET home page. */
 router.route('/').get(displayIndex);
 router.route('/window/:id').delete(deleteRecord).put(editRecord);
 router.route('/window/add').post(addRecord);
 router.route('/register').post(register);
+router.route('/login').post(login);
 
 
 module.exports = router;
