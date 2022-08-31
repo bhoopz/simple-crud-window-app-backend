95e1f381f60f9d1e86a046d92d2d4132792d1820
Dominik Filip <filu4444@gmail.com>
2022-08-31 15:16:24 +0200

    changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors())
-
+app.set('trust proxy', 1);
 app.use(sessions({
   secret: process.env.SESSION_SECRET,
   saveUninitialized: false,
