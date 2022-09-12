dbd79eb440cf8950abebbe5e0e9fff1b1cd5cbed
Dominik Filip <filu4444@gmail.com>
2022-09-12 17:38:05 +0200

    tests

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,7 +56,7 @@ const login = async (req, res) => {
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["x-access-token"];
   console.log(authHeader);
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(".")[1];
   console.log(token);
   if (token == null) res.status(401);
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
